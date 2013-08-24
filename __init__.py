49db7304f7026531978b13031abbfe3fc92a113d
Joe Cross <joe.mcross@gmail.com>
2013-08-24 11:38:18 -0700

    Move kmeans calculations to c, drop speed/min_diff options
    Performance on a 4269x2867 image went from ~60s to 2.9s
    Now I need to figure out how to package this porperly

--- __init__.py
+++ __init__.py
@@ -1,20 +1,9 @@
 import os
 import time
-import random
 import webbrowser
-from itertools import izip
 from contextlib import contextmanager
+from dropper import lib
 from PIL import Image
-INFINITY = float("Inf")
-
-
-class Point(object):
-    __slots__ = "rgb", "w", "i"
-
-    def __init__(self, rgb, w, i):
-        self.rgb = rgb
-        self.w = w
-        self.i = i
 
 @contextmanager
 def timer(msg=None):
@@ -24,7 +13,7 @@ def timer(msg=None):
     stop = time.clock()
     print msg.format(stop - start)
 
-def full_render(src, dst, k, speed, min_diff):
+def full_render(src, dst, k, seed):
     '''
     k is number of colors
     min_diff is termination point for clustering algorithm
@@ -33,74 +22,26 @@ def full_render(src, dst, k, speed, min_diff):
     src = os.path.expanduser(src)
     dst = os.path.expanduser(dst)
     with timer("Full render: {}"):
-        print "Getting colors"
-        colors = dominant_colors(src, k, speed, min_diff)
-        print "Rendering"
+        colors = dominant_colors(src, k, seed)
         render_colors(colors, src, dst)
 
-def dominant_colors(path, k, speed, min_diff):
+def dominant_colors(path, k, seed):
     print "Loading image"
-    image = open_image(path, speed)
+    image = Image.open(path)
     print "Calculating points"
     pts = points(image)
     print "Calculating centers"
-    centers = kmeans(pts, k, min_diff)
-    print "Stepping values"
-    return [map(int, center.rgb) for center in centers]
-
-def open_image(path, speed):
-    image = Image.open(path)
-    w, h = image.size
-    speed = float(speed)
-    size = int(w / speed), int(h / speed)
-    image.thumbnail(size)
-    return image
-
-def points(image):
-    w, h = image.size
-    print "{} pixels".format(w * h)
-    pts = [Point(rgb=colors, w=count, i=-1) for count, colors in image.getcolors(w * h)]
-    print "{} unique colors".format(len(pts))
-    return pts
-
-def kmeans(points, k, min_diff):
-    '''Only returns centers, not clustered points'''
-    # Initial centers
-    centers = random.sample(points, k)
-
-    iterations = 0
-    while True:
-        iterations += 1
-        # Assignment step
-        for point in points:
-            min_dist = INFINITY
-            for center_index, center in enumerate(centers):
-                dist = distance2(point, center)
-                if dist < min_dist:
-                    min_dist = dist
-                    point.i = center_index
-        # Update step
-        diff = 0
-        for center_index in xrange(k):
-            old = centers[center_index]
-            cluster_points = [point for point in points if point.i == center_index]
-            new = cluster_center(cluster_points)
-            centers[center_index] = new
-            diff = max(diff, distance2(old, new))
-        if diff <= min_diff:
-            break
-    print "{} assign/update iterations".format(iterations)
+    centers = lib.kmeans(pts, k, seed)
     return centers
 
-def distance2(p1, p2):
-    return sum((p1.rgb[i] - p2.rgb[i]) ** 2 for i in xrange(3))
-
-def cluster_center(points):
-   n = sum(p.w for p in points)
-   point_colors = list([p.w * p.rgb[0], p.w * p.rgb[1], p.w * p.rgb[2]] for p in points)
-   vals = map(sum, izip(*point_colors))
-   vals = [v / n for v in vals]
-   return Point(rgb=vals, w=1, i=-1)
+def points(image):
+    points = []
+    w, h = image.size
+    for count, colors in image.getcolors(w * h):
+        rgbc = list(colors)
+        rgbc.append(count)
+        points.append(rgbc)
+    return points
 
 def clamp(x):
     return max(0, min(x, 255))
@@ -108,24 +49,19 @@ def clamp(x):
 def rgb_to_hex(rgb):
     return "#{0:02x}{1:02x}{2:02x}".format(*rgb)
 
-
 def render_colors(colors, src, dst):
-    print "Loading template"
+    print "Saving Image"
     base = os.path.join(here(), 'base.html')
     with open(base) as f:
         template = f.read()
-    print "Creating divs"
     row = "<div class='box' style='background-color: {}'></div>"
     src_row = "<div class='box src'><img src='{}'></div>"
-    print "Sorting by value"
     colors = value_sort(colors)
-    print "Mapping to hex"
     colors = map(rgb_to_hex, colors)
     rows = [row.format(c) for c in colors]
     rows.append(src_row.format(src))
     pre, post = template.split("{{content}}")
     render = pre + '\n'.join(rows) + post
-    print "Saving to file"
     with open(dst, 'w') as f:
         f.write(render)
     webbrowser.open(dst)
--- create.py
+++ create.py
@@ -1,24 +1,20 @@
 """Dropper.
 
 Usage:
-  create.py <src> <dst> [--colors=<COLORS>] [--speed=<SPEED>] [--min-delta=<DELTA>]
+  create.py <src> <dst> [--colors=<COLORS>] [--seed=<SEED>]
 
 Options:
   -h --help                    Show this screen.
   -c COLORS --colors=<COLORS>  Number of generated colors.  [default: 5]
-  -s SPEED --speed=<SPEED>     Inverse scale factor.  1 is no scaling, 2 cuts dimensions in half, etc. [default: 2]
-  --min-delta=<DELTA>          Center drift cutoff to terminate k-mean algorithm.  Small numbers are slower, less variance.  [default: 1]
+  -s SEED --seed=<SEED>        Seed for initial mean selection. [default: 12345]
 """
 from dropper import full_render
 from docopt import docopt
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-
     src = arguments['<src>']
     dst = arguments['<dst>']
     k = max(1, int(arguments['--colors']))
-    min_diff = max(0, float(arguments['--min-delta']))
-    speed = max(1, int(arguments['--speed']))
-
-    full_render(src, dst, k, speed, min_diff)
+    seed = int(arguments['--seed'])
+    full_render(src, dst, k, seed)
